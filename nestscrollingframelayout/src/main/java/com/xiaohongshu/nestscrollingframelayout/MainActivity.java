8dc8d981b0487ae08989b87e1da998c2c80dffc5
wupengjian <pwu@xiaohongshu.com>
2017-12-15 17:58:16 +0800

    update：更新控制台代码

--- nestscrollingframelayout/src/main/java/com/xiaohongshu/nestscrollingframelayout/MainActivity.java
+++ nestscrollingframelayout/src/main/java/com/xiaohongshu/nestscrollingframelayout/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements IPrint {
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity implements IPrint {
     private TextView mNestedInfoTv;
     private ParentView mParentView;
     private ChildView mChildView;
+    private ScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements IPrint {
         mParentView.setPrint(this);
         mChildView = (ChildView) findViewById(R.id.child_view);
         mChildView.setPrint(this);
+
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
     }
 
     public void clearInfo(View view) {
@@ -32,13 +36,6 @@ public class MainActivity extends AppCompatActivity implements IPrint {
     public void printInfo(String text) {
         String content = String.format("%03d: %s \n", mNestedInfoTv.getLineCount(), text);
         mNestedInfoTv.append(content);
-        textViewScroll2Bottom(mNestedInfoTv);
-    }
-
-    private void textViewScroll2Bottom(TextView textView) {
-        int offset = textView.getLineCount() * textView.getLineHeight();
-        if (offset > (textView.getHeight() - textView.getLineHeight() - 20)) {
-            textView.scrollTo(0, offset - textView.getHeight() + textView.getLineHeight() + 20);
-        }
+        mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
     }
 }
